8c26ab3f56b5ee23e84cc7353ca98b52354e149c
Manu B - <emm.bernard@gmail.com>
2021-02-06 20:20:55 +0100

    control walking + animation

---
+++ src/walkingcontrol/js/player.js
@@ -0,0 +1,115 @@
+/** @module Player */
+
+import * as THREE from "three";
+
+import Controller from "./controller";
+
+export default class Player {
+  constructor(options) {
+    this._player = null;
+    this._body = null;
+    this._physic = null;
+
+    this._isMoving = false;
+    this.animations = null;
+
+    this.options = { speed: 0.1, ...options };
+
+    this._initPlayer();
+    this._initBody();
+
+    this.controller = new Controller();
+  }
+
+  addBody(body) {
+    this._body = body;
+    this._player.add(body);
+  }
+
+  /**
+   * Get player
+   */
+  get() {
+    return this._player;
+  }
+
+  /**
+   * Get player body
+   */
+  getBody() {
+    return this._body;
+  }
+
+  /**
+   * Move the player in the stage
+   */
+  onTick() {
+    const mz = this.controller.movingZ[0];
+    const mx = this.controller.movingX[0];
+    let calculatedSpeedBasedOnAngle = this.options.speed;
+    if (mz || mx) {
+      if (!this._isMoving) {
+        calculatedSpeedBasedOnAngle = 0.8;
+        this.animations.run();
+      }
+      this._isMoving = true;
+    } else {
+      if (this._isMoving) {
+        this.animations.iddle();
+      }
+      this._isMoving = false;
+    }
+    if (mz) {
+      const vector =
+        mz == "up" ? -calculatedSpeedBasedOnAngle : calculatedSpeedBasedOnAngle;
+      this._player.translateZ(vector);
+    }
+
+    if (mx) {
+      const vector =
+        mx == "left"
+          ? -calculatedSpeedBasedOnAngle
+          : calculatedSpeedBasedOnAngle;
+      this._player.translateX(vector);
+    }
+
+    // Body Rotation
+    if (mz == "down" && !mx) {
+      this._body.rotation.y = 0;
+    } else if (mz == "down" && mx == "right") {
+      this._body.rotation.y = Math.PI * 0.25;
+    } else if (mx == "right" && !mz) {
+      this._body.rotation.y = Math.PI * 0.5;
+    } else if (mz == "up" && mx == "left") {
+      this._body.rotation.y = Math.PI * 1.25;
+    } else if (mz == "up" && !mx) {
+      this._body.rotation.y = Math.PI;
+    } else if (mz == "up" && mx == "right") {
+      this._body.rotation.y = Math.PI * 0.75;
+    } else if (mx == "left" && !mz) {
+      this._body.rotation.y = Math.PI * 1.5;
+    } else if (mz == "down" && mx == "left") {
+      this._body.rotation.y = Math.PI * 1.75;
+    }
+  }
+
+  /**
+   * Initialize player
+   * @Private
+   */
+  _initPlayer() {
+    this._player = new THREE.Mesh(
+      new THREE.BoxGeometry(0.3, 0.3, 0.3),
+      new THREE.MeshStandardMaterial({ color: "red", wireframe: true })
+    );
+
+    this._player.name = "Player";
+    this._player.userData.preserve = true;
+  }
+
+  /**
+   * Initialize player body
+   * @Private
+   */
+  _initBody() {}
+}
---
+++ src/walkingcontrol/script.js
@@ -0,0 +1,145 @@
+import "./style.css";
+import * as THREE from "three";
+
+import imageSource from "./textures/1.png";
+
+import "./js/init.js";
+
+import { textureLoader } from "./js/textureLoader.js";
+import { scene } from "./js/scene.js";
+import { animations } from "./js/animation.js";
+import { gui } from "./js/debug.js";
+
+import Player from "./js/player";
+
+const matcapTexture = textureLoader.load(imageSource);
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const gltfLoader = new GLTFLoader();
+
+/**
+ * Debug
+ */
+const debug = {
+  walk: function () {
+    switchAction("walk");
+  },
+  run: function () {
+    switchAction("run");
+  },
+  tmode: function () {
+    switchAction("tmode");
+  },
+  iddle: function () {
+    switchAction("iddle");
+  },
+};
+
+gui.add(debug, "walk");
+gui.add(debug, "run");
+gui.add(debug, "tmode");
+gui.add(debug, "iddle");
+
+/**
+ * Player
+ */
+const player = new Player();
+
+/**
+ * Animations
+ */
+let currentAction = null;
+const actions = {};
+const step = 0.1;
+
+const switchAction = (to) => {
+  currentAction = actions[to];
+};
+
+gltfLoader.load("/3d/girl.gltf", (gltf) => {
+  const mixer = new THREE.AnimationMixer(gltf.scene);
+
+  animations.push(function (elapsedTime, deltaTime) {
+    mixer.update(deltaTime);
+  });
+
+  actions.iddle = mixer.clipAction(gltf.animations[0]);
+  actions.walk = mixer.clipAction(gltf.animations[3]);
+  actions.run = mixer.clipAction(gltf.animations[1]);
+  actions.tmode = mixer.clipAction(gltf.animations[2]);
+
+  currentAction = actions.iddle;
+
+  animations.push(function (elapsedTime, deltaTime) {
+    const currentActionWeight = currentAction.getEffectiveWeight();
+
+    if (currentActionWeight < 1) {
+      currentAction.setEffectiveWeight(currentActionWeight + step);
+    }
+
+    for (let key in actions) {
+      const otherAction = actions[key];
+      if (otherAction != currentAction) {
+        const otherActionWeight = otherAction.getEffectiveWeight();
+        if (otherActionWeight > 0) {
+          otherAction.setEffectiveWeight(otherActionWeight - step);
+        }
+      }
+    }
+  });
+
+  for (let key in actions) {
+    const action = actions[key];
+    action.play();
+  }
+
+  actions.iddle.setEffectiveWeight(1);
+  actions.walk.setEffectiveWeight(0);
+  actions.run.setEffectiveWeight(0);
+  actions.tmode.setEffectiveWeight(0);
+
+  player.addBody(gltf.scene);
+  player.animations = debug;
+
+  scene.add(player.get());
+
+  gltf.scene.traverse(function (object) {
+    if (object.isMesh) object.castShadow = true;
+  });
+
+  animations.push(function (elapsedTime, deltaTime) {
+    player.onTick();
+  });
+});
+
+/**
+ * Lights
+ */
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+hemiLight.position.set(0, 20, 0);
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff);
+dirLight.shadow.mapSize.set(1024, 1024);
+dirLight.position.set(-3, 10, -10);
+dirLight.castShadow = true;
+dirLight.shadow.camera.top = 25;
+dirLight.shadow.camera.bottom = -25;
+dirLight.shadow.camera.left = -25;
+dirLight.shadow.camera.right = 25;
+dirLight.shadow.camera.near = 0.1;
+dirLight.shadow.camera.far = 140;
+scene.add(dirLight);
+
+/**
+ * Objects
+ */
+const floorGeometry = new THREE.CylinderGeometry(105, 105, 0.1, 32);
+const floorMaterial = new THREE.MeshPhongMaterial({ color: "grey" });
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.receiveShadow = true;
+
+scene.add(floor);
+scene.background = new THREE.Color(0xa0a0a0);
+scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
