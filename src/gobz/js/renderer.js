f5a5ae315e165a48453d4e83e6360489e01c6448
Manu B - <emm.bernard@gmail.com>
2021-02-13 20:01:58 +0100

    Goblin s first steps

---
+++ src/gobz/js/renderer.js
@@ -0,0 +1,21 @@
+import * as THREE from "three";
+import { canvas } from "./canvas.js";
+import { sizes } from "./sizes.js";
+import * as dat from "dat.gui";
+
+/**
+ * Renderer
+ */
+export const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 2.8;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
